e2697ec895b71af97286bbca5fe6733571f66289
Falren <nokuchie@gmail.com>
2020-10-22 14:16:09 +0300

    Rspec create and delete

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
-describe 'Task API', type: :request do
+describe 'GET #index', type: :request do
   it 'returns all tasks' do
     get '/tasks'
 
     expect(response).to have_http_status(:ok)
-    #expect(JSON.parse(response.body).size).to eq(1)
   end
 end
+
+
+describe 'POST #create', type: :request do
+  it 'posts a task' do
+    post '/tasks', params: {task: {title: '123', description: '12345', active: true}}
+    expect(response).to have_http_status(:created)
+  end
+end
+
+
+describe 'DELETE #destroy', type: :request do
+  it 'deletes a task' do
+    delete '/tasks/1'
+    expect(response).to have_http_status(:accepted)
+  end
+end
+
