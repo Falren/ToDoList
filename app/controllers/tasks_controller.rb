cec2d1b0b26a87fe951e1d40e8f35d59bbdba376
Falren <nokuchie@gmail.com>
2020-10-22 13:22:09 +0300

    add create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,31 @@
 class TasksController < ApiController
 
-  before_action :find_task, except: :index
+  before_action :find_task, except: %i[index create]
   before_action :find_tasks, only: :index
 
   def create
-
+    @task = Task.new(task_params)
+    if @task.save
+      render json: {status: 'success', message: 'Task has been created', data: @task }, status: :created
+    else
+      render json: {status: 'error', message: 'Task is not created', data: @task.errors }, status: :unprocessable_entity
+    end
   end
 
   def update
+    @task.save(task_params)
   end
 
   def show
-    render json: { task: @task }
+    render json: { data: @task }
   end
 
   def index
-    render json: { tasks: @tasks }
+    render json: { data: @tasks }
   end
 
   def delete
+
   end
 
   private
