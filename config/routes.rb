5212303eac885d66ca7bae464831b847ec593487
Falren <nokuchie@gmail.com>
2020-10-25 11:04:20 +0200

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, except: :new
-      root 'tasks#index'
     end
   end
+  root 'api/v1/tasks#index'
 end
