e95d56d87928dd97d9754cad85a01128860267f5
Falren <nokuchie@gmail.com>
2020-11-04 17:58:18 +0200

    My first commit

--- db/migrate/20201022080007_create_tasks.rb
+++ db/migrate/20201022080007_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :title
       t.string :description
-      t.boolean :active, default: true
+      t.boolean :active
 
       t.timestamps
     end
