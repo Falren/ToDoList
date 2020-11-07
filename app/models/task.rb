960e09e6304aaef0bc983074143e024ae3758901
Falren <nokuchie@gmail.com>
2020-11-07 23:02:27 +0200

    refactor && add to_boolean method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ApplicationRecord
+  
   validates_presence_of :title
 
-  scope :by_active, ->(value) { where(active: JSON.parse(value)) unless value.nil? }
+  scope :by_active, ->(value) { where(active: to_boolean(value)) unless value.nil? }
   scope :search, ->(value) { where('title ILIKE :query OR description ILIKE :query', query: "%#{value.downcase}%") unless value.blank? }
+
+  private 
+
+  def self.to_boolean(string)
+    ActiveRecord::Type::Boolean.new.cast(string)
+  end
 end
